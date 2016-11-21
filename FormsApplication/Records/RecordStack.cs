356dd48af49985db0830244519aa15a12683737f
Michael Park <mpark4656@gmail.com>
2016-11-21 12:12:34 -0500

    Addd Event Handler for Mouse Click
    Added SelectAll event action for index box mouse click

---
+++ FormsApplication/Records/RecordStack.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections.Generic;
+
+namespace FormsApplication.Records
+{
+    /// <summary>
+    ///  RecordStack is a collection of Records.
+    ///  The more interaction user creates, the larger RecordStack gets
+    /// </summary>
+    public class RecordStack
+    {
+        private List<Record> records;
+
+        /// <summary>
+        ///  Default Constructor
+        /// </summary>
+        public RecordStack()
+        {
+            records = new List<Record>();
+        }
+
+        /// <summary>
+        ///  Public getter and setter for List<Record> records
+        /// </summary>
+        public List<Record> Records
+        {
+            get { return records; }
+            set { records = value; }
+        }
+
+        /// <summary>
+        ///  Method to allow users to use square bracket (indexer) to access
+        ///  elements in List<Record> records
+        /// </summary>
+        /// <param name="index">
+        ///  int index at which to access and modify the Record
+        /// </param>
+        public Record this[int index]
+        {
+            // Allows Read
+            get
+            {
+                return records[index];
+            }
+
+            // Allows Modification
+            set
+            {
+                records[index] = value;
+            }
+        }
+
+        /// <summary>
+        ///  Method that returns the size of records
+        /// </summary>
+        /// <returns>
+        ///  count int number of records in the list
+        /// </returns>
+        public int Size()
+        {
+            return records.Count;
+        }
+
+        /// <summary>
+        ///  Method to remove a record at given index
+        /// </summary>
+        /// <param name="index">
+        ///  index int index at which to remove the record
+        /// </param>
+        public void RemoveAt( int index)
+        {
+            records.RemoveAt(index);
+        }
+
+        /// <summary>
+        ///  Method to add a new record to List<Record> records
+        /// </summary>
+        /// <param name="toAdd">
+        ///  Record record to add
+        /// </param>
+        public void Add( Record toAdd )
+        {
+            records.Add(toAdd);
+        }  
+    }
+}
